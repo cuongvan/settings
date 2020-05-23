4cda7932f1f96bf607d24d2eb7ede84af806d07e
Cuong Van <cuongvtien@gmail.com>
2020-05-23 23:13:31 +0700

    sync .bashrc & .bash_profile

---
+++ install.sh
@@ -0,0 +1,38 @@
+RCDIR=$(realpath rcfiles)
+
+function link_file() {
+    git_file=$1     # absolute path
+    dest_file=$2    # absolute path
+
+    if [[ -e $dest_file && -L $dest_file ]]; then # healthy symlink
+        echo "    $dest_file is linked already"
+    elif [[ -e $dest_file && ! -L $dest_file ]]; then # normal file, not a symlink
+        echo "    $dest_file is not a symlink"
+        echo "        > Move ==> ${dest_file}.bak"
+        mv $dest_file ${dest_file}.bak
+        echo "        > Link"
+        ln -s $git_file $dest_file
+    elif [[ -L $dest_file ]]; then # is symlink but real file not exists
+        echo "    $dest_file link is broken. Relink now"
+        rm $dest_file
+        ln -s $git_file $dest_file
+    else
+        echo "    $dest_file not found. Link now"
+        ln -s $git_file $dest_file
+    fi
+}
+
+# link ~/.bashrc
+echo "> Link ~/.bashrc"
+link_file $RCDIR/bashrc $HOME/.bashrc
+
+
+# link ~/.bash_profile
+echo "> Link ~/.bash_profile"
+if [[ -e $HOME/.profile ]]; then
+    echo "    Found .profile. Will not use this file"
+    echo "        > move ==> $HOME/.profile.bak"
+    mv $HOME/.profile $HOME/.profile.bak
+fi
+
+link_file $RCDIR/bash_profile $HOME/.bash_profile
